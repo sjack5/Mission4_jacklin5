2b58d9a4aeaf760afb4b75653bf7f5fa027f312d
Spencer Jacklin <sjacklin01@gmail.com>
2023-02-06 20:25:54 -0700

    Mission 5, the whole thing hehe

---
+++ Mission4_jacklin5/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_jacklin5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_jacklin5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator(GradeCalculatorModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission4_jacklin5/Models/GradeCalculatorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_jacklin5.Models
+{
+    public class GradeCalculatorModel
+    {
+        [Range(0, 100)]
+        public byte Assignments { get; set; }
+        public byte Project { get; set; }
+        public byte Quiz { get; set; }
+        public byte Midterm { get; set; }
+        public byte Final { get; set; }
+        public byte INTEX { get; set; }
+
+    }
+}
